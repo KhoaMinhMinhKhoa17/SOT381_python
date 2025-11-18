7c5f4539141c584d85e72e518aceaa16b2816a5f
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-11-18 12:48:27 +0700

    Update doi_do_c_thanh_do_k.py

--- doi_do_c_thanh_do_k.py
+++ doi_do_c_thanh_do_k.py
@@ -1,3 +1,4 @@
-c = int(input('Nhập vào nhiệt độ C : '))
+c = int(input('Nhập vào nhiệt độ Celsius : '))
 k = c + 273.15
-print('Nhiệt độ K đã được quy đổi là :', k )
+
+print('Nhiệt độ Kelvin đã được quy đổi là :', k )
