e797365543a6534b0df419c2af11baddd7e1c9f6
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-11-18 13:56:23 +0700

    Add files via upload

---
+++ tinh_trung_binh_cong.py
@@ -0,0 +1,6 @@
+print('Tính trung bình cộng của ba số bất kì')
+a = float(input('Nhập vào số thứ nhất : '))
+b = float(input('Nhập vào số thức hai : '))
+c = float(input('Nhập vào số thứ ba : '))
+TBC = (a + b + c) / 3
+print('Vậy trung bình cộng là :', TBC )
