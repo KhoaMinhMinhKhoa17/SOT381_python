00d42ff00f3ee84e07712213b3072cee65ab2bab
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-09 17:44:04 +0700

    Edit

--- tinh_trung_binh_cong.py
+++ tinh_trung_binh_cong.py
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 print('Tính trung bình cộng của ba số bất kì')
 a = float(input('Nhập vào số thứ nhất : '))
 b = float(input('Nhập vào số thức hai : '))
 c = float(input('Nhập vào số thứ ba : '))
 TBC = (a + b + c) / 3
-=======
 print('Tính trung bình cộng của ba số bất kì')
 a = float(input('Nhập vào số thứ nhất : '))
 b = float(input('Nhập vào số thức hai : '))
 c = float(input('Nhập vào số thứ ba : '))
 TBC = (a + b + c) / 3
->>>>>>> 7990ea80439d54f8e4d29ea545e6ac02ae2032fe
 print('Vậy trung bình cộng là :', TBC )
