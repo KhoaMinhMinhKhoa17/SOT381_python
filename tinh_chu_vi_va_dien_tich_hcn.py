ab3096c2f0f13779331271e3995b71574459f934
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-09 17:42:25 +0700

    Edit

--- tinh_chu_vi_va_dien_tich_hcn.py
+++ tinh_chu_vi_va_dien_tich_hcn.py
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 a = int(input('Nhập vào chiều dài của hình chữ nhật :'))
 b = int(input('Nhập vào chiều rộng của hình chủ nhật :'))
 P = (a+b)*2
 S = a*b
 print('Chu vi hình chủ nhật là : ', P)
-=======
 a = int(input('Nhập vào chiều dài của hình chữ nhật :'))
 b = int(input('Nhập vào chiều rộng của hình chủ nhật :'))
 P = (a+b)*2
 S = a*b
 print('Chu vi hình chủ nhật là : ', P)
->>>>>>> 7990ea80439d54f8e4d29ea545e6ac02ae2032fe
 print('Diện tích hình chủ nhật là : ', S )
