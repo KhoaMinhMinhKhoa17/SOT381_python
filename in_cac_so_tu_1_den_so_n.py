2cd062f4db86c23af35a4ff1e01f4487befd2f6f
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-09 17:41:05 +0700

    Edit

--- in_cac_so_tu_1_den_so_n.py
+++ in_cac_so_tu_1_den_so_n.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 n = int(input('Hãy nhập vào số n : '))
 for i in range(1,n+1) :
     print(i, end=' ')
-=======
 n = int(input('Hãy nhập vào số n : '))
 for i in range(1,n+1) :
 
     print(i, end=' ')
-
->>>>>>> 7990ea80439d54f8e4d29ea545e6ac02ae2032fe
