88d3d214051df759085c1d2dc0dec8886a5af0b1
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-09 17:41:39 +0700

    Edit

--- in_so_chan_tu_1_den_n.py
+++ in_so_chan_tu_1_den_n.py
@@ -1,4 +1,10 @@
-<<<<<<< HEAD
+n = int(input('Hãy nhập vào số n : '))
+for i in range(1,n+1) :
+    if i%2==0 :
+        print(i, end=' ')
+print('')
+for i in range(2,n+1,2):
+    print(i, end=' ')
 n = int(input('Hãy nhập vào số n : '))
 for i in range(1,n+1) :
     if i%2==0 :
@@ -7,15 +13,3 @@ print('')
 for i in range(2,n+1,2):
     print(i, end=' ')
 
-    
-=======
-n = int(input('Hãy nhập vào số n : '))
-for i in range(1,n+1) :
-    if i%2==0 :
-        print(i, end=' ')
-print('')
-for i in range(2,n+1,2):
-    print(i, end=' ')
-
-    
->>>>>>> 7990ea80439d54f8e4d29ea545e6ac02ae2032fe
