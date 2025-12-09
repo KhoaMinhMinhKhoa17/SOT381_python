c83c57571766dd3ed3f3b15fd70f9747920df1a9
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-09 17:43:29 +0700

    Edit
    Cleaned up the Python script by removing merge conflict markers.

--- tinh_tong_cac_so_tu_1_den_n.py
+++ tinh_tong_cac_so_tu_1_den_n.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 n = int(input('Hãy nhập vào số n : '))
 tong=0
 for i in range(1,n+1) :
     tong=tong+i
-=======
 n = int(input('Hãy nhập vào số n : '))
 tong=0
 for i in range(1,n+1) :
     tong=tong+i
->>>>>>> 7990ea80439d54f8e4d29ea545e6ac02ae2032fe
 print(f'Tổng các số từ 1 đến {n} là: {tong}')
