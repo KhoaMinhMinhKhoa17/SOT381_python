12ca5b18855e513e8bed20b2cb962afa5f3f8e58
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-15 14:58:37 +0700

    Add files via upload

---
+++ bai_6.py
@@ -0,0 +1,9 @@
+username = input('Hãy nhập vào tên đăng nhập :')
+while username != 'Y' and username !='y':
+    print('Login Failed')
+    
+    username=input('Please refill :')
+print('Login Success')
+    
+     
+            
