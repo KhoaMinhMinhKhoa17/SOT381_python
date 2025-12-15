ed9766c657e83d89e610f73298b932f5a4f5cabc
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-15 13:54:36 +0700

    Add files via upload

---
+++ tinh_cv_dt.py
@@ -0,0 +1,6 @@
+a = int(input('Nhập vào chiều rộng a của hcn : '))
+b = int(input('Nhập vào chiều dài của hcn : '))
+P = ( a + b) *2
+S = a*b
+print('Chu vi của hcn là : ', P )
+print('Diện tích của hcn là :, S ')
