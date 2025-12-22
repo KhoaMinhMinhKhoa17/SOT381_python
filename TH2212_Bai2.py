9632f1d6cc6e352be34882e15e15a3ed44959f29
KhoaMinhMinhKhoa17 <khoa.tlm.67khmt@ntu.edu.vn>
2025-12-22 14:54:42 +0700

    Add files via upload

---
+++ TH2212_Bai2.py
@@ -0,0 +1,9 @@
+a = int(input('Nhap vao so thu 1 :'))
+b = int(input('Nhap vao so thu 2:'))
+c  = int(input('Nhap vao so thu 3:'))
+max = a
+if b > max :
+    max = b 
+if c > max :
+    max = c
+print('So lon nhat la : ', max)
